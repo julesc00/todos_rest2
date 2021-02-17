b41e4177113c4b5ba45adb7e1136969be3b1133b
juliobriones <julesc00@hotmail.com>
2021-02-17 17:17:46 -0600

    Added corsheaders.

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,22 @@
+
+?
+todos/models.py,6/e/6e30a84e234c3e3d9ccfd0b468fc4ecfe4306189
+>
+todos/admin.py,4/4/441ffab18743e3ab3e2e3c30ef56362d15b6bd9e
+D
+todos/serializers.py,e/d/ed2890d44fa89758caa03abbfee90b74077d8b4e
+>
+todos/views.py,9/c/9c6114cf7336609f4916821ff3fc20037814e092
+=
+todos/urls.py,b/5/b520df04fc49c200ab7dd668111b025f97fbe22a
+D
+todo_project/urls.py,8/6/860bc2c41bd47c11e22acdbf4871f30e2385ffba
+9
+	readme.md,2/7/275d783e298228506068436512433d343feb52aa
+7
+LICENSE,0/3/0398ccd0f49298b10a3d76a47800d2ebecd49859
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+H
+todo_project/settings.py,7/d/7d9a15c3ef17377b3953fc7c50d982a75d82a430
--- todo_project/settings.py
+++ todo_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
 
     'todos.apps.TodosConfig',
 ]
@@ -52,11 +53,17 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+    "http://localhost:8000",
 ]
 
 ROOT_URLCONF = 'todo_project.urls'
