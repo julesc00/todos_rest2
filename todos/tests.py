083652a14bc4b9a0d48b74b469acc0baaf4a0742
juliobriones <julesc00@hotmail.com>
2021-02-17 17:26:06 -0600

    Added tests for title and body creation in models.py.

--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Todo
+
+
+class TodoModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Todo.objects.create(title="first todo", body="a body here")
+
+    def test_title_content(self):
+        """Test the title is created."""
+        todo = Todo.objects.get(id=1)
+        expected_object_name = f"{todo.title}"
+
+        self.assertEqual(expected_object_name, "first todo")
+
+    def test_body_content(self):
+        """Test the body content is created."""
+        todo = Todo.objects.get(id=1)
+        expected_object_name = f"{todo.body}"
+
+        self.assertEqual(expected_object_name, "a body here")
